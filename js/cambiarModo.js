1e4188f01e15d52ca6e0060f544ee50cbbc89882
Carlos Caruci <carlosalbertocarucilinarez>
2022-09-24 18:30:16 -0400

    Se implemento la funcion de cambio de variables en el root (css) para que pueda cambiar de modo al hacer click.

--- js/cambiarModo.js
+++ js/cambiarModo.js
@@ -4,13 +4,16 @@ function cambiarDeModo(){
 
 let opcionesModo = document.querySelectorAll(".mode_li");
 
+//Aqui se llama al root del css para poder modificarlo
 let cssRoot = document.querySelector(':root');
 
    opcionesModo.forEach(element => {
     element.addEventListener('click', ()=>{
-        
+        //Si se le da click en el algun modo, se cambiara los estilos directamente del :root
+
         if (element.classList[1]==="mode__dark") {
-            console.log('se activa modo dark')   
+            
+        //con los metodos style.setProperty se pueden acceder a las propiedades del :root para modificarlas
             cssRoot.style.setProperty('--color-principal', 'rgb(70, 0, 0)'); 
             cssRoot.style.setProperty('--color-secundario', 'rgb(26, 0, 0)'); 
             cssRoot.style.setProperty('--color-contraste', 'rgb(36, 36, 36)'); 
@@ -19,10 +22,9 @@ let cssRoot = document.querySelector(':root');
             cssRoot.style.setProperty('--color-transparente-oscuro', 'rgba(0, 0, 0, 0.2)');         
             cssRoot.style.setProperty('--color-transparente-claro', 'rgba(161, 161, 161, 0.2)');
             cssRoot.style.setProperty('--letras-style', '"Bungee Spice", cursive');
-            
+            console.log('se activa modo dark')   
         
         }else if (element.classList[1]==="mode__elegant") { 
-
 
             cssRoot.style.setProperty('--color-principal', 'rgb(248, 0, 0)'); 
             cssRoot.style.setProperty('--color-secundario', 'rgb(255, 255, 255)'); 
@@ -33,9 +35,11 @@ let cssRoot = document.querySelector(':root');
             cssRoot.style.setProperty('--color-transparente-claro', 'rgb(97, 97, 97)'); 
             cssRoot.style.setProperty('--color-letra', 'rgb(46, 13, 13)');
             cssRoot.style.setProperty('--letras-style', '"Luckiest Guy", cursive');
+            console.log('se activa modo elegante');
+
         }
         else if (element.classList[1]==="mode__office") {
-            console.log('se activa modo oficina')
+            
             cssRoot.style.setProperty('--color-principal', 'rgb(187, 163, 82)'); 
             cssRoot.style.setProperty('--color-secundario', 'rgb(119, 111, 1)'); 
             cssRoot.style.setProperty('--color-contraste', 'rgb(0, 0, 0)'); 
@@ -45,11 +49,11 @@ let cssRoot = document.querySelector(':root');
             cssRoot.style.setProperty('--color-transparente-claro', 'rgb(155, 2, 2)'); 
             cssRoot.style.setProperty('--color-letra', 'rgb(163, 158, 140)');
             cssRoot.style.setProperty('--letras-style', '"Akaya Kanadaka",cursive');
-            console.log('se activa modo elegante') 
+            console.log('se activa modo office'); 
 
         }
         else if (element.classList[1]==="mode__night") {
-            console.log('se activa modo noche')            
+
             cssRoot.style.setProperty('--color-principal', 'rgb(0, 0, 0)'); 
             cssRoot.style.setProperty('--color-secundario', 'rgb(0, 0, 0)'); 
             cssRoot.style.setProperty('--color-contraste', 'rgb(29, 29, 29)'); 
@@ -59,11 +63,11 @@ let cssRoot = document.querySelector(':root');
             cssRoot.style.setProperty('--color-transparente-claro', 'rgb(155, 2, 2)'); 
             cssRoot.style.setProperty('--color-letra', 'rgb(124, 124, 124)');
             cssRoot.style.setProperty('--letras-style', '"Rubik Dirt", cursive');
+            console.log('se activa modo noche');
 
         }
         else if (element.classList[1]==="mode__rosa") {
-            console.log('se activa modo rosa')       
-            
+       
             cssRoot.style.setProperty('--color-principal', 'rgb(255, 143, 236)'); 
             cssRoot.style.setProperty('--color-secundario', 'rgb(253, 251, 113)'); 
             cssRoot.style.setProperty('--color-contraste', 'rgb(255, 187, 0)'); 
@@ -73,12 +77,8 @@ let cssRoot = document.querySelector(':root');
             cssRoot.style.setProperty('--color-transparente-claro', 'rgb(155, 2, 2)'); 
             cssRoot.style.setProperty('--color-letra', 'rgb(255, 0, 212)');
             cssRoot.style.setProperty('--letras-style', '"Courgette", cursive');
-
-
-            
+            console.log('se activa modo rosa')            
         }
-        
-    
     })
 })
 }
